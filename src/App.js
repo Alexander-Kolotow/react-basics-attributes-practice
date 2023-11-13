bdec2cdafd53f3b1040752619dc43abc1bb883a4
Alexander Kolotow <alexkolotow@gmail.com>
2023-11-13 11:42:14 +0100

    solved third task

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import React from "react";
 import "./styles.css";
 
+function Article () {
+  return (
+    <article className="article">
+    <h2 className="article__title">Climate change</h2>
+    <label htmlFor="content">
+      <input id="content" />
+      <a className="article__link" href="https://en.wikipedia.org/wiki/Climate_change">
+        learn more here...
+      </a>
+    </label>
+  </article>
+);
+}
+
 export default function App() {
-  return <div>Replace me with an article</div>;
+  return < Article /> ;
 }
