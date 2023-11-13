114c4cfe9b42adf86212ca8e979a42ecd68e01ce
Alexander-Kolotow <147614805+Alexander-Kolotow@users.noreply.github.com>
2023-11-13 11:17:43 +0000

    update app.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function Article () {
     <article className="article">
     <h2 className="article__title">Climate change</h2>
     <label htmlFor="content">
-      <input id="content" type="button" />
+      <input id="content" type="text" />
       <a className="article__link" href="https://en.wikipedia.org/wiki/Climate_change">
         learn more here...
       </a>
